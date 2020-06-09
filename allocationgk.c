22c39baae445b87bc35d17f057c33e689a50870a
prateekgupta <kphprateek@gmail.com>
2020-06-10 01:31:35 +0530

    allocationgk

---
+++ allocationgk.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c,temp,f,g,h=1;
+	
+	int d,count=0;
+	scanf("%d",&a);
+	
+	while(a!=0)
+	{
+	  scanf("%d",&f);
+	  int ar[f];
+	  scanf("%d",&d);
+	  count=0;
+	  for(g=0;g<f;g++)
+	  {
+	      scanf("%d",&ar[g]);
+	  }
+	
+
+	mergesort(ar,0,f-1);
+	
+	
+	
+		
+	
+	for(b=0;b<f;b++)
+	    {
+	        
+	        if(ar[b]<=d)
+	        {
+	            count++;
+	            d=d-ar[b];
+	        }
+	    }
+	    printf("case #%d",h);
+	    printf(": %d  \n",count);
+	    a--;
+	    h++;
+	}
+	
+	return 0;
+}
+void mergesort(int a[],int i,int j)
+{
+	int mid;
+		
+	if(i<j)
+	{
+		mid=(i+j)/2;
+		mergesort(a,i,mid);		
+		mergesort(a,mid+1,j);	
+		merge(a,i,mid,mid+1,j);	
+	}
+}
+ 
+void merge(int a[],int i1,int j1,int i2,int j2)
+{
+	int temp[100001];
+	int i,j,k;
+	i=i1;	
+	j=i2;	
+	k=0;
+	
+	while(i<=j1 && j<=j2)
+	{
+		if(a[i]<a[j])
+			temp[k++]=a[i++];
+		else
+			temp[k++]=a[j++];
+	}
+	
+	while(i<=j1)	
+		temp[k++]=a[i++];
+		
+	while(j<=j2)
+		temp[k++]=a[j++];
+		
+	
+	for(i=i1,j=0;i<=j2;i++,j++)
+		a[i]=temp[j];
+}
